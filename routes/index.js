c29a816aee20fe3bac9c83ea5c0d5588af2ad937
ignacioborraz <ignacioborraz@hotmail.com>
2023-04-17 15:36:12 -0300

    animals

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ let express = require('express')
 let router = express.Router()
 
 const events = require('./events')
-const eventsMH = require('./eventsMH')
 const evento = require('./eventos')
 const producto = require('./producto')
+const animales = require('./animales')
 
 router.get('/', function(_req, res, _next) {
   res.render('index', { title: 'MH API' })
 });
 
 router.use('/amazing', events)
-router.use('/amazing-events', eventsMH)
+router.use('/animales', animales)
 router.use('/espectaculares', evento)
 router.use('/mindy', producto)
 
