ae687ac464d388f7cc843dc5b2a4b4b3cc3eafa7
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-04-19 23:21:05 -0300

    minga (#7)

---
+++ models/data/minga/Chapter.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema(
+    {
+        manga_id:{ type:mongoose.Types.ObjectId, ref:'mangas', required:true },
+        title: { type:String, required:true },
+        cover_photo: { type: String, required: true },
+        pages: { type:Array, required:true },
+        order: { type:Number, required:true }
+    },{
+        timestamps: true
+    }
+)
+
+export const Chapter = mongoose.model('chapters', schema)
